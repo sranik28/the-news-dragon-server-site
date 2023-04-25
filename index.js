d92d358cf75d118cd668d2dd14e2fe00fa8076d9
sranik28~ <anikshariarrahman@gmail.com>
2023-04-25 21:24:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const categories = require("./Data/Categories.json");
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Dragon is running')
+});
+
+app.get("/categories", (req, res) => {
+    res.send(categories)
+});
+
+app.listen(port, () => {
+    console.log(`Dragon api is running on port: ${port}`)
+})
